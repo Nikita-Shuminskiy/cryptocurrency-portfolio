d12a4d9c77897a56a44764a9572b3f82fc4753ad
nick-gansta <shuminskiy.nik@mail.ru>
2022-01-16 01:02:43 +0300

    fix reducer crypt

--- src/Bll/Crypt-coin-list-reducer.ts
+++ src/Bll/Crypt-coin-list-reducer.ts
@@ -1,39 +1,44 @@
+import { api } from '../Dal/Api';
 import { CryptocurrencyListType, DataChartType } from '../Dal/Types';
 import { setAppStatus } from './App-reducer';
-import { api } from '../Dal/Api';
-import { ActionsTypes, AppDispatchType } from './Store/Store';
+import { AppDispatchType } from './Store/Store';
 
 const initialState = {
-    totalAssetData: [] as CryptocurrencyListType[],
-    dataAssetsPortion: [] as CryptocurrencyListType[],
-    timestamp: null as number | null,
-    chartData: [] as DataChartType[],
-    topAssets: [] as CryptocurrencyListType[],
+  totalAssetData: [] as CryptocurrencyListType[],
+  dataAssetsPortion: [] as CryptocurrencyListType[],
+  timestamp: null as number|null,
+  chartData: [] as DataChartType[],
+  topAssets: [] as CryptocurrencyListType[],
 
 }
 
 export const cryptocurrencyReducer = (state = initialState, action: ActionsCryptoCurrencyTypes): CryptocurrencyInitType => {
-    switch (action.type) {
-        case 'CRYPT/SET-CURRENT-ASSETS-PORTION':
-            return {...state, dataAssetsPortion: action.data}
-        case 'CRYPT/SET-DATA-CHART':
-            return {...state, chartData: action.data}
-        case 'CRYPT/SET-ASSETS-TOTAL':
-            return {...state, totalAssetData: action.totalAssetData, timestamp: action.timestamp, topAssets: action.totalAssetData.slice(0,3)}
-        default: {
-            return state
-        }
+  switch (action.type) {
+    case 'CRYPT/SET-CURRENT-ASSETS-PORTION':
+      return {...state, dataAssetsPortion: action.data}
+    case 'CRYPT/SET-DATA-CHART':
+      return {...state, chartData: action.data}
+    case 'CRYPT/SET-ASSETS-TOTAL':
+      return {
+        ...state,
+        totalAssetData: action.totalAssetData,
+        timestamp: action.timestamp,
+        topAssets: action.totalAssetData.slice(0, 3),
+      }
+    default: {
+      return state
     }
+  }
 }
 //action
 export const setDataAssetsPortion = (data: CryptocurrencyListType[]) => {
-    return {type: 'CRYPT/SET-CURRENT-ASSETS-PORTION', data} as const
+  return {type: 'CRYPT/SET-CURRENT-ASSETS-PORTION', data} as const
 }
 export const setTotalAssetData = (totalAssetData: CryptocurrencyListType[], timestamp: number) => {
-    return {type: 'CRYPT/SET-ASSETS-TOTAL', totalAssetData, timestamp} as const
+  return {type: 'CRYPT/SET-ASSETS-TOTAL', totalAssetData, timestamp} as const
 }
 export const setDataChart = (data: DataChartType[]) => {
-    return {type: 'CRYPT/SET-DATA-CHART', data} as const
+  return {type: 'CRYPT/SET-DATA-CHART', data} as const
 }
 export const setTopDataAssets = (data: CryptocurrencyListType[]) => {
     return {type: 'CRYPT/SET-TOP-DATA-ASSETS', data} as const
@@ -52,31 +57,31 @@ export const getDataAssetsTC = () => async (dispatch: AppDispatchType) => {
     }
 }
 export const getDataAssetsTotalTC = () => async (dispatch: AppDispatchType) => {
-    dispatch(setAppStatus('loading'))
-    try {
-        const response = await api.getAssets()
-        const {data, timestamp} = response.data
-        dispatch(setTotalAssetData(data, timestamp))
-        dispatch(setAppStatus('succeeded'))
-    } catch (e) {
-        dispatch(setAppStatus('failed'))
-    }
+  dispatch(setAppStatus('loading'))
+  try {
+    const response = await api.getAssets()
+    const {data, timestamp} = response.data
+    dispatch(setTotalAssetData(data, timestamp))
+    dispatch(setAppStatus('succeeded'))
+  } catch (e) {
+    dispatch(setAppStatus('failed'))
+  }
 }
 export const getChartDataTC = (id: string) => async (dispatch: AppDispatchType) => {
-    dispatch(setAppStatus('loading'))
-    try {
-        const response = await api.getChartAssetData(id)
-        const {data} = response.data
-        dispatch(setDataChart(data))
-        dispatch(setAppStatus('succeeded'))
-    } catch (e) {
-        dispatch(setAppStatus('failed'))
-    }
+  dispatch(setAppStatus('loading'))
+  try {
+    const response = await api.getChartAssetData(id)
+    const {data} = response.data
+    dispatch(setDataChart(data))
+    dispatch(setAppStatus('succeeded'))
+  } catch (e) {
+    dispatch(setAppStatus('failed'))
+  }
 }
 
 //types
 export type CryptocurrencyInitType = typeof initialState
 export type ActionsCryptoCurrencyTypes =
-    | ReturnType<typeof setDataAssetsPortion>
-    | ReturnType<typeof setDataChart>
-    | ReturnType<typeof setTotalAssetData>
+  |ReturnType<typeof setDataAssetsPortion>
+  |ReturnType<typeof setDataChart>
+  |ReturnType<typeof setTotalAssetData>
