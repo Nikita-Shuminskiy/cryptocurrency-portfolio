7e93583cac5c140e158aa19dc66fd31352a90eb3
nick-gansta <shuminskiy.nik@mail.ru>
2022-02-28 00:38:45 +0300

    added array all route for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,50 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { Table } from './Component/Table/Table';
 import { Header } from './Component/Header/Header';
 import { Portfolio } from './Component/Portfolio/Portfolio';
 import { CurrencyInfo } from './Component/Table/CurrencyInfo/CurrencyInfo';
+import { Table } from './Component/Table/Table';
 
-const PATH = {
-    HOME: '/'
+export type AllPatchType = {
+  patch: string
+  component: JSX.Element
 }
+const PATH = {
+  home: '/',
+  portfolio: '/portfolio',
+  table: '/table',
+  currInfo: '/currency-info/:id',
+}
+export const allRoute: Array<AllPatchType> = [
+  {
+    patch: PATH.portfolio,
+    component: <Portfolio/>,
+  },
+  {
+    patch: PATH.table,
+    component: <Table/>,
+  },
+  {
+    patch: PATH.currInfo,
+    component: <CurrencyInfo/>,
+  },
+]
 
 const App = () => {
-    return (
-        <div className="container">
-            <Header/>
-                    <Switch>
-                        <Route exact path={PATH.HOME} render={() => <Redirect to={'/table'}/>}/>
-                        <Route path={'/portfolio'} render={() => <Portfolio/>}/>
-                        <Route path={'/table'} render={() => <Table/>}/>
-                        <Route path={'/currency-info/:id'} render={() => <CurrencyInfo/>}/>
-                    </Switch>
-        </div>
-    );
+  return (
+    <div className="container">
+      <Header/>
+      <Switch>
+        <Route exact path={PATH.home} render={() => <Redirect to={'/table'}/>}/>
+        {
+          allRoute.map((route) => {
+            return <Route path={route.patch} render={() => route.component}/>
+          })
+        }
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/Component/Table/Table.tsx
+++ src/Component/Table/Table.tsx
@@ -1,35 +1,34 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import './Table.scss'
-import { Assets } from './Asset/Assets';
 import { RequestStatusType } from '../../Bll/App-reducer';
-import { Preloader } from '../Common/Preloader/Preloader';
-import { AlertError } from '../Common/AlertError/AlertError';
-import { CryptocurrencyListType } from '../../Dal/Types';
-import { Paginator } from '../Common/Paginator/Paginator';
 import { AppStateType } from '../../Bll/Store/Store';
+import { CryptocurrencyListType } from '../../Dal/Types';
+import { AlertError } from '../Common/AlertError/AlertError';
+import { Paginator } from '../Common/Paginator/Paginator';
+import { Preloader } from '../Common/Preloader/Preloader';
+import { Assets } from './Asset/Assets';
+import './Table.scss'
 
 export const Table = () => {
-    const status = useSelector<AppStateType, RequestStatusType>(state => state.app.status)
-    const totalAssetData = useSelector<AppStateType, CryptocurrencyListType[]>((state) => state.cryptocurrencyList.dataAssetsPortion)
-    const errorReqest = 'The server is overloaded, try again later'
-    return <div className="table-assets">
-        {status === 'failed' && <AlertError error={errorReqest}/>}
-        <div className="table-assets__header">
-            <p className="table-assets__text">Rank</p>
-            <p className="table-assets__text">Rank</p>
-            <p className="table-assets__text">Price</p>
-        </div>
-        <div className="table-assets__body">
-            <input type="text"/>
-            {
-                status === 'loading' ? <Preloader/>
-                    :
-                    totalAssetData.map((asset) => {
-                        return <Assets key={asset.id} item={asset}/>
-                    })
-            }
-        </div>
-         <Paginator/>
+  const status = useSelector<AppStateType, RequestStatusType>(state => state.app.status)
+  const totalAssetData = useSelector<AppStateType, CryptocurrencyListType[]>((state) => state.cryptocurrencyList.dataAssetsPortion)
+  const errorReqest = 'The server is overloaded, try again later'
+  return <div className="table-assets">
+    {status === 'failed' && <AlertError error={errorReqest}/>}
+    <div className="table-assets__header">
+      <p className="table-assets__text">Rank</p>
+      <p className="table-assets__text">Rank</p>
+      <p className="table-assets__text">Price</p>
     </div>
+    <div className="table-assets__body">
+      {
+        status === 'loading' ? <Preloader/>
+          :
+          totalAssetData.map((asset) => {
+            return <Assets key={asset.id} item={asset}/>
+          })
+      }
+    </div>
+    <Paginator/>
+  </div>
 }
